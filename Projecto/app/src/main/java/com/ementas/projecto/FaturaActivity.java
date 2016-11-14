fcb42fd4fc65776ed61237e0716f6120e1bdc6a8
André <xadden@gmail.com>
2016-11-14 12:51:03 +0000

    did some things

---
+++ Projecto/app/src/main/java/com/ementas/projecto/FaturaActivity.java
@@ -0,0 +1,15 @@
+package com.ementas.projecto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FaturaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fatura);
+
+
+    }
+}
